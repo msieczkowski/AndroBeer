1745b301cacc9826de72c498da33c23e770f40a2
msieczkowski <maxime.sieczkowski@etu.univ-lyon1.fr>
2018-01-25 10:02:40 +0100

    listing beer ok

--- app/src/main/java/com/example/lp/beer/MainActivity.java
+++ app/src/main/java/com/example/lp/beer/MainActivity.java
@@ -2,83 +2,117 @@ package com.example.lp.beer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Will show the string "data" that holds the results
-    TextView results;
-    // URL of object to be parsed
-    String JsonURL = "http://192.168.240.4:3000/beer";
-    // This string will hold the results
-    String data = "";
-    // Defining the Volley request queue that handles the URL request concurrently
-    RequestQueue requestQueue;
+   // EditText etGitHubUser; // This will be a reference to our GitHub username input.
+    //Button btnGetRepos;  // This is a reference to the "Get Repos" button.
+    TextView tvRepoList;  // This will reference our repo list text box.
+    RequestQueue requestQueue;  // This is our requests queue to process our HTTP requests.
+
+    String baseUrl = "http://192.168.240.4:3000/beer/";  // This is the API base URL (GitHub API)
+    String url;  // This will hold the full URL which will include the username entered in the etGitHubUser.
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        // Creates the Volley request queue
-        requestQueue = Volley.newRequestQueue(this);
+        super.onCreate(savedInstanceState);  // This is some magic for Android to load a previously saved state for when you are switching between actvities.
+        setContentView(R.layout.activity_main);  // This links our code to our layout which we defined earlier.
 
-        // Casts results into the TextView found within the main layout XML with id jsonData
-        results = (TextView) findViewById(R.id.jsonData);
+        //this.etGitHubUser = (EditText) findViewById(R.id.et_github_user);  // Link our github user text box.
+        //this.btnGetRepos = (Button) findViewById(R.id.btn_get_repos);  // Link our clicky button.
+        this.tvRepoList = (TextView) findViewById(R.id.tv_repo_list);  // Link our repository list text output box.
+        this.tvRepoList.setMovementMethod(new ScrollingMovementMethod());  // This makes our text box scrollable, for those big GitHub contributors with lots of repos :)
 
-        // Creating the JsonObjectRequest class called obreq, passing required parameters:
-        //GET is used to fetch data from the server, JsonURL is the URL to be fetched from.
-        JsonObjectRequest obreq = new JsonObjectRequest(Request.Method.GET, JsonURL, null,
-                // The third parameter Listener overrides the method onResponse() and passes
-                //JSONObject as a parameter
-                new Response.Listener<JSONObject>() {
+        requestQueue = Volley.newRequestQueue(this);  // This setups up a new request queue which we will need to make HTTP requests.
+        this.getRepoList();
+    }
 
-                    // Takes the response from the JSON request
+    private void clearRepoList() {
+        // This will clear the repo list (set it as a blank string).
+        this.tvRepoList.setText("");
+    }
+
+    private void addToRepoList(String Name, String Price) {
+        // This will add a new repo to our list.
+        // It combines the repoName and lastUpdated strings together.
+        // And then adds them followed by a new line (\n\n make two new lines).
+        String strRow = Name + " / " + Price + " €";
+        String currentText = tvRepoList.getText().toString();
+        this.tvRepoList.setText(currentText + "\n\n" + strRow);
+    }
+
+    private void setRepoListText(String str) {
+        // This is used for setting the text of our repo list box to a specific string.
+        // We will use this to write a "No repos found" message if the user doens't have any.
+        this.tvRepoList.setText(str);
+    }
+
+    private void getRepoList() {
+        // First, we insert the username into the repo url.
+        // The repo url is defined in GitHubs API docs (https://developer.github.com/v3/repos/).
+        this.url = this.baseUrl;
+
+        // Next, we create a new JsonArrayRequest. This will use Volley to make a HTTP request
+        // that expects a JSON Array Response.
+        // To fully understand this, I'd recommend readng the office docs: https://developer.android.com/training/volley/index.html
+        JsonArrayRequest arrReq = new JsonArrayRequest(Request.Method.GET, url,
+                new Response.Listener<JSONArray>() {
                     @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            JSONObject obj = response.getJSONObject("colorObject");
-                            // Retrieves the string labeled "colorName" and "description" from
-                            //the response JSON Object
-                            //and converts them into javascript objects
-                            String color = obj.getString("colorName");
-                            String desc = obj.getString("description");
+                    public void onResponse(JSONArray response) {
+                        // Check the length of our response (to see if the user has any repos)
+                        if (response.length() > 0) {
+                            // The user does have repos, so let's loop through them all.
+                            for (int i = 0; i < response.length(); i++) {
+                                try {
+                                    // For each repo, add a new line to our repo list.
+                                    JSONObject jsonObj = response.getJSONObject(i);
+                                    String Name = jsonObj.get("name").toString();
+                                    String Price = jsonObj.get("price").toString();
 
-                            // Adds strings from object to the "data" string
-                            data += "Color Name: " + color +
-                                    "nDescription : " + desc;
+                                    addToRepoList(Name, Price);
+                                } catch (JSONException e) {
+                                    // If there is an error then output this to the logs.
+                                    Log.e("Volley", "Invalid JSON Object.");
+                                }
 
-                            // Adds the data string to the TextView "results"
-                            results.setText(data);
+                            }
+                        } else {
+                            // The user didn't have any repos.
+                            setRepoListText("No repos found.");
                         }
-                        // Try and catch are included to handle any errors due to JSON
-                        catch (JSONException e) {
-                            // If an error occurs, this prints the error to the log
-                            e.printStackTrace();
-                        }
+
                     }
                 },
-                // The final parameter overrides the method onErrorResponse() and passes VolleyError
-                //as a parameter
+
                 new Response.ErrorListener() {
                     @Override
-                    // Handles errors that occur due to Volley
                     public void onErrorResponse(VolleyError error) {
-                        Log.e("Volley", "Error");
+                        // If there a HTTP error then add a note to our repo list.
+                        setRepoListText("Error while calling REST API");
+                        Log.e("Volley", error.toString());
                     }
                 }
         );
-        // Adds the JSON object request "obreq" to the request queue
-        requestQueue.add(obreq);
+        // Add the request we just defined to our request queue.
+        // The request queue will automatically handle the request as soon as it can.
+        requestQueue.add(arrReq);
     }
 }
