c930f77748449773ee54261b236f907b0721b48e
José Santiago <jose.santiago42400@gmail.com>
2018-03-01 13:19:20 +0100

    edit with data

--- app/src/main/java/com/example/lp/beer/EditBeerActivity.java
+++ app/src/main/java/com/example/lp/beer/EditBeerActivity.java
@@ -11,8 +11,14 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,6 +33,75 @@ public class EditBeerActivity extends AppCompatActivity {
         Intent intent = getIntent();
         final String beerId  = intent.getStringExtra(BeerDetailsActivity.EXTRA_MESSAGE);
         beer = beerId;
+        String requestUrl = "http://192.168.240.4:3000/beer/";
+        requestUrl +=  beerId;
+
+        final EditText BeerNameField = (EditText) findViewById(R.id.editTextName);
+        final EditText BeerDescriptionField = (EditText) findViewById(R.id.editTextDescription);
+        final EditText BeerOriginField = (EditText) findViewById(R.id.editTextOrigin);
+        final EditText BeerTypeField = (EditText) findViewById(R.id.editTextType);
+        final EditText BeerPriceField = (EditText) findViewById(R.id.editTextPrice);
+        final EditText BeerDegreeField = (EditText) findViewById(R.id.editTextDegree);
+        final EditText BeerImgUrlField = (EditText) findViewById(R.id.editTextImgUrl);
+
+        RequestQueue requestQueue;
+        requestQueue = Volley.newRequestQueue(this);
+
+        JsonArrayRequest arrReq = new JsonArrayRequest(Request.Method.GET, requestUrl,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        // Check the length of our response (to see if the user has any repos)
+                        if (response.length() > 0) {
+                            // The user does have repos, so let's loop through them all.
+
+                            for (int i = 0; i < response.length(); i++) {
+                                try {
+                                    // For each repo, add a new line to our repo list.
+                                    JSONObject jsonObj = response.getJSONObject(i);
+                                    String Id = beerId;
+                                    String Name = jsonObj.get("name").toString();
+                                    String Degree = jsonObj.get("degree").toString();
+                                    String Description = jsonObj.get("description").toString();
+                                    String Origin = jsonObj.get("origin").toString();
+                                    String Price = jsonObj.get("price").toString();
+                                    String Type = jsonObj.get("type").toString();
+                                    String ImgUrl = jsonObj.get("img_url").toString();
+                                    Beer aBeer = new Beer(Id, Name, Degree, Description, Origin, Price, Type, ImgUrl);
+                                    BeerNameField.setText(aBeer.getName());
+                                    BeerTypeField.setText(aBeer.getType());
+                                    BeerDescriptionField.setText(aBeer.getDescription());
+                                    BeerDegreeField.setText(aBeer.getDegree());
+                                    BeerPriceField.setText(aBeer.getPrice());
+                                    BeerOriginField.setText(aBeer.getOrigin());
+                                    BeerImgUrlField.setText(aBeer.getImgUrl());
+
+                                } catch (JSONException e) {
+                                    // If there is an error then output this to the logs.
+                                    Log.e("Volley", "Invalid JSON Object.");
+                                }
+
+                            }
+                        } else {
+                            // The user didn't have any repos.
+                            //setRepoListText("No repos found.");
+                        }
+
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // If there a HTTP error then add a note to our repo list.
+                        //setRepoListText("Error while calling REST API");
+                        Log.e("Volley", error.toString());
+                    }
+                }
+        );
+        // Add the request we just defined to our request queue.
+        // The request queue will automatically handle the request as soon as it can.
+        requestQueue.add(arrReq);
     }
 
     public void editBeer(View view){
